fcd859de7989e4360ff29b90eec2543d83760563
flavio.santana <9090flavio@gmail.com>
2022-11-01 16:03:29 -0300

    feat: derived query

--- src/main/java/io/com/rh/repository/FuncionarioRepository.java
+++ src/main/java/io/com/rh/repository/FuncionarioRepository.java
@@ -1,9 +1,18 @@
 package io.com.rh.repository;
 
 import io.com.rh.entity.Funcionario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FuncionarioRepository  extends CrudRepository<Funcionario, Long> {
+
+    List<Funcionario> findByNome(String nome);
+
+    @Query("SELECT f FROM Funcionario f JOIN FETCH f.cargo c WHERE f.nome = :nome")
+    List<Funcionario> findByNomeFetchCliente(String nome);
+
 }
--- src/test/java/io/com/rh/repository/FuncionarioRepositoryTest.java
+++ src/test/java/io/com/rh/repository/FuncionarioRepositoryTest.java
@@ -4,12 +4,14 @@ import com.github.javafaker.Faker;
 import io.com.rh.entity.Cargo;
 import io.com.rh.entity.Funcionario;
 import io.com.rh.entity.Unidade;
+import org.hibernate.LazyInitializationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -48,7 +50,91 @@ class FuncionarioRepositoryTest {
         funcionario.setCargo(cargo);
         funcionario.addUnidade(unidade);
         funcionarioRepository.save(funcionario);
+    }
 
+    @Test
+    void deveriaBuscarFuncionarioPorNone(){
+
+        Cargo cargo = new Cargo();
+        cargo.setDescricao(faker.job().title());
+        cargoRepository.save(cargo);
+
+        Unidade unidade = new Unidade();
+        unidade.setDescricao(faker.address().streetAddress());
+        unidade.setEndereco(faker.address().fullAddress());
+        unidadeRepository.save(unidade);
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setCpf(faker.idNumber().valid());
+        funcionario.setNome(faker.name().fullName());
+        funcionario.setDataContratacao(LocalDateTime.now());
+        funcionario.setSalario(new BigDecimal(faker.number().randomDouble(2, 1000, 5000)));
+        funcionario.setCargo(cargo);
+        funcionario.addUnidade(unidade);
+        funcionarioRepository.save(funcionario);
+
+
+        List<Funcionario> buscaPorNome = funcionarioRepository.findByNome(funcionario.getNome());
+        assertNotNull(buscaPorNome);
+    }
+
+    @Test
+    void deveriaDarExcecaoDeLazyInitializationExceptionAoDarGetEmCargo(){
+
+        Cargo cargo = new Cargo();
+        cargo.setDescricao(faker.job().title());
+        cargoRepository.save(cargo);
+
+        Unidade unidade = new Unidade();
+        unidade.setDescricao(faker.address().streetAddress());
+        unidade.setEndereco(faker.address().fullAddress());
+        unidadeRepository.save(unidade);
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setCpf(faker.idNumber().valid());
+        funcionario.setNome(faker.name().fullName());
+        funcionario.setDataContratacao(LocalDateTime.now());
+        funcionario.setSalario(new BigDecimal(faker.number().randomDouble(2, 1000, 5000)));
+        funcionario.setCargo(cargo);
+        funcionario.addUnidade(unidade);
+        funcionarioRepository.save(funcionario);
+
+
+        List<Funcionario> buscaPorNome = funcionarioRepository.findByNome(funcionario.getNome());
+        assertNotNull(buscaPorNome);
+        assertThrows(LazyInitializationException.class, () -> {
+            buscaPorNome.stream().findFirst().ifPresent(f -> f.getCargo().getDescricao());
+
+        });
+
+    }
+
+    @Test
+    void deveriaBuscarFuncionarioPorNoneComFetchEmCliente(){
+
+        Cargo cargo = new Cargo();
+        cargo.setDescricao(faker.job().title());
+        cargoRepository.save(cargo);
+
+        Unidade unidade = new Unidade();
+        unidade.setDescricao(faker.address().streetAddress());
+        unidade.setEndereco(faker.address().fullAddress());
+        unidadeRepository.save(unidade);
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setCpf(faker.idNumber().valid());
+        funcionario.setNome(faker.name().fullName());
+        funcionario.setDataContratacao(LocalDateTime.now());
+        funcionario.setSalario(new BigDecimal(faker.number().randomDouble(2, 1000, 5000)));
+        funcionario.setCargo(cargo);
+        funcionario.addUnidade(unidade);
+        funcionarioRepository.save(funcionario);
+
+
+        List<Funcionario> buscaPorNome = funcionarioRepository.findByNomeFetchCliente(funcionario.getNome());
+        assertNotNull(buscaPorNome);
+        assertNotNull(buscaPorNome.get(0).getCargo());
+        assertEquals(buscaPorNome.get(0).getCargo().getDescricao(), cargo.getDescricao());
     }
 
 }
